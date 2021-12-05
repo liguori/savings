f913f23a9b85107889c43385a02599c42ba93477
Gianluigi Liguori <gianluigiliguori@live.it>
2021-12-05 18:46:53 +0100

    Migrate Blazor to .NET 6 With Minimal API

--- src/SavingsProjection.SPA/Program.cs
+++ src/SavingsProjection.SPA/Program.cs
@@ -1,57 +1,45 @@
+
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Radzen;
 using Refit;
 using SavingsProjection.Model;
+using SavingsProjection.SPA;
 using SavingsProjection.SPA.Authorization;
 using SavingsProjection.SPA.Services;
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
 
-namespace SavingsProjection.SPA
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("app");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<CustomAuthorizationMessageHandler>();
+
+var configuredAuthentication = builder.Configuration["AuthenticationToUse"];
+
+if (configuredAuthentication == AuthenticationToUse.AzureAD)
 {
-    public class Program
+    builder.Services.AddMsalAuthentication(options =>
     {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddScoped<DialogService>();
-            builder.Services.AddScoped<NotificationService>();
-            builder.Services.AddScoped<CustomAuthorizationMessageHandler>();
-
-            var configuredAuthentication = builder.Configuration["AuthenticationToUse"];
-
-            if (configuredAuthentication == AuthenticationToUse.AzureAD)
-            {
-                builder.Services.AddMsalAuthentication(options =>
-                {
-                    options.ProviderOptions.LoginMode = "redirect";
-                    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
-                    options.ProviderOptions.DefaultAccessTokenScopes.Add(builder.Configuration["AzureAd:DefaultScope"]);
-                });
-            }
-
-            var httpClientBuilder = builder.Services.AddRefitClient<ISavingProjectionApi>().ConfigureHttpClient((sp, c) =>
-            {
-                c.BaseAddress = new Uri(builder.Configuration["SavingProjectionApiServiceUrl"]);
-                if (configuredAuthentication == AuthenticationToUse.ApiKey)
-                {
-                    c.DefaultRequestHeaders.Add("X-API-Key", builder.Configuration["ApiKey"]);
-                }
-            });
-
-
-            if (configuredAuthentication == AuthenticationToUse.AzureAD)
-            {
-                httpClientBuilder.AddHttpMessageHandler<CustomAuthorizationMessageHandler>();
-            }
-
-            await builder.Build().RunAsync();
-        }
-    }
+        options.ProviderOptions.LoginMode = "redirect";
+        builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+        options.ProviderOptions.DefaultAccessTokenScopes.Add(builder.Configuration["AzureAd:DefaultScope"]);
+    });
 }
+
+var httpClientBuilder = builder.Services.AddRefitClient<ISavingProjectionApi>().ConfigureHttpClient((sp, c) =>
+{
+    c.BaseAddress = new Uri(builder.Configuration["SavingProjectionApiServiceUrl"]);
+    if (configuredAuthentication == AuthenticationToUse.ApiKey)
+    {
+        c.DefaultRequestHeaders.Add("X-API-Key", builder.Configuration["ApiKey"]);
+    }
+});
+
+
+if (configuredAuthentication == AuthenticationToUse.AzureAD)
+{
+    httpClientBuilder.AddHttpMessageHandler<CustomAuthorizationMessageHandler>();
+}
+
+await builder.Build().RunAsync();
