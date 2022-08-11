333a574cb458161a8a6c149ec0d8585555d28bd8
Gianluigi Liguori <giliguor@microsoft.com>
2022-08-11 17:38:46 +0200

    Add first MAUI support

---
+++ src/Savings.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Savings.MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ src/Savings.MAUI/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Savings.MAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
