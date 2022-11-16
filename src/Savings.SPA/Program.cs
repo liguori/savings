c960778c800de82ae9d0dd80052967c850d1c390
Gianluigi Liguori <giliguor@microsoft.com>
2022-11-16 13:55:54 +0100

    Add Custom Authorization Message Handler

--- src/Savings.SPA/Program.cs
+++ src/Savings.SPA/Program.cs
@@ -16,6 +16,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<DialogService>();
 builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<CustomAuthorizationMessageHandler>();
 
 var configuredAuthentication = builder.Configuration["AuthenticationToUse"];
 
