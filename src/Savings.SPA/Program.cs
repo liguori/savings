a9c5e548834a4293651d2be0d777b9171ada25b0
Gianluigi Liguori <giliguor@microsoft.com>
2022-11-16 20:53:54 +0100

    Remove cookie

--- src/Savings.SPA/Program.cs
+++ src/Savings.SPA/Program.cs
@@ -25,7 +25,6 @@ if (configuredAuthentication == AuthenticationToUse.AzureAD)
     builder.Services.AddMsalAuthentication(options =>
     {
         options.ProviderOptions.Cache.CacheLocation = "localStorage";
-        options.ProviderOptions.Cache.StoreAuthStateInCookie = true;
         options.ProviderOptions.LoginMode = "redirect";
         builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
         options.ProviderOptions.DefaultAccessTokenScopes.Add(builder.Configuration["AzureAd:DefaultScope"]);
