c7d5c9b0b3cdbdc74e206ca7b51507d0cd63ea40
Gianluigi Liguori <gianluigiliguori@live.it>
2024-08-26 16:12:48 +0200

    Add .NET Aspire

--- src/Savings.SPA/Program.cs
+++ src/Savings.SPA/Program.cs
@@ -9,6 +9,7 @@ using Savings.SPA.Authorization;
 using Savings.SPA.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
