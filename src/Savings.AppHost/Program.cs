d4664fefbb6d90c80d0f43c7cf2c9af13d0700b5
Gianluigi Liguori <gianluigiliguori@live.it>
2024-09-01 13:33:44 +0200

    Disable proxy for WASM

--- src/Savings.AppHost/Program.cs
+++ src/Savings.AppHost/Program.cs
@@ -6,6 +6,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 builder.AddProject<Projects.Savings_API>("api");
 
 builder.AddProject<Projects.Savings_SPA>("client")
-    .WithExternalHttpEndpoints();
+    .WithEndpoint("https", endpoint => endpoint.IsProxied = false); //Disable the proxy in order to allow the WASM debugger to work on the same port configured in the launchSettings.json
 
 builder.Build().Run();
