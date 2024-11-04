e83c6fcb42f7701f68d279144000e6971f67d914
Gianluigi Liguori <gianluigiliguori@live.it>
2024-11-04 10:50:03 +0100

    Edit the scope for the applicaiton

--- src/Savings.SPA/Program.cs
+++ src/Savings.SPA/Program.cs
@@ -28,6 +28,7 @@ if (configuredAuthentication == AuthenticationToUse.AzureAD)
         options.ProviderOptions.Cache.CacheLocation = "localStorage";
         options.ProviderOptions.LoginMode = "redirect";
         builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+        options.ProviderOptions.DefaultAccessTokenScopes.Add("https://graph.microsoft.com/User.Read");
         options.ProviderOptions.DefaultAccessTokenScopes.Add(builder.Configuration["AzureAd:DefaultScope"]!);
     });
 }
